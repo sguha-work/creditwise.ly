2ea78aecdc0b343debed3fb43d23838d4a513946
Sahasrangshu Guha <sguha1988.life@gmail.com>
2026-04-12 17:12:52 +0530

    Change base path from '/creditwise.ly/' to '/creditwisely/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/creditwise.ly/',
+  base: '/creditwisely/',
   plugins: [react()],
   envPrefix: ['VITE_', 'FIREBASE_'],
   build: {
